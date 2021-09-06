d63311f2b921219a07ab7a07c9977cbf435644e3
elzup <guild0105@gmail.com>
2021-09-06 18:14:12 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -4,40 +4,24 @@ type User = {
   name: string
   skill: [number, number]
   rank: number
-  nth: number
-}
-type UserCalc = Omit<User, 'skill'> & {
-  skill: [number, number]
 }
 
-const sum = (a, b) => a + b
-function patchUserCalc(user: User, nth: number): UserCalc {
-  return {
-    ...user,
-    skill: [user.skill, user.skill / 3],
-  }
-}
-
-function patchUsersCalc(users: User[]): UserCalc[] {
-  const total = users.map((v) => v.skill).reduce(sum, 0)
-  const avg = total / users.length
-
-  return users.map(patchUserCalc)
-}
 function calc(users0: User[]) {
   const users = [...users0]
   trueskill.AdjustPlayers(users)
   return users
 }
 
+const initSkill = 400
 function main() {
   const users: User[] = [
-    { name: 'alice', skill: [400, 400 / 3], rank: 1 },
-    { name: 'bob', skill: [400, 400 / 3], rank: 2 },
-    { name: 'chris', skill: [400, 400 / 3], rank: 3 },
-    { name: 'darren', skill: [400, 400 / 3], rank: 4 },
-    { name: 'elis', skill: [400, 400 / 3], rank: 5 },
+    { name: 'alice', skill: [initSkill, initSkill / 3], rank: 1 },
+    { name: 'bob', skill: [initSkill, initSkill / 3], rank: 2 },
+    { name: 'chris', skill: [initSkill, initSkill / 3], rank: 3 },
+    { name: 'darren', skill: [initSkill, initSkill / 3], rank: 4 },
+    { name: 'elis', skill: [initSkill, initSkill / 3], rank: 5 },
   ]
+  console.log(calc(users))
   console.log(calc(users))
 }
 
