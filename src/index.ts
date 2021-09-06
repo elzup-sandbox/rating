8af550d3565c729e91f5c7e2a9cad52aa17b92b5
elzup <guild0105@gmail.com>
2021-09-06 18:02:37 +0900

    patch

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,41 @@ import trueskill from 'trueskill'
 
 type User = {
   name: string
-  skill: number[]
+  skill: [number, number]
   rank: number
+  nth: number
+}
+type UserCalc = Omit<User, 'skill'> & {
+  skill: [number, number]
 }
 
+const sum = (a, b) => a + b
+function patchUserCalc(user: User, nth: number): UserCalc {
+  return {
+    ...user,
+    skill: [user.skill, user.skill / 3],
+  }
+}
+
+function patchUsersCalc(users: User[]): UserCalc[] {
+  const total = users.map((v) => v.skill).reduce(sum, 0)
+  const avg = total / users.length
+
+  return users.map(patchUserCalc)
+}
 function calc(users0: User[]) {
   const users = [...users0]
   trueskill.AdjustPlayers(users)
   return users
 }
 
-const base = 1000
 function main() {
   const users: User[] = [
-    { name: 'alice', skill: [400, base], rank: 1 },
-    { name: 'bob', skill: [400, base], rank: 2 },
-    { name: 'chris', skill: [400, base], rank: 3 },
-    { name: 'darren', skill: [400, base], rank: 4 },
-    { name: 'elis', skill: [400, base], rank: 5 },
+    { name: 'alice', skill: [400, 400 / 3], rank: 1 },
+    { name: 'bob', skill: [400, 400 / 3], rank: 2 },
+    { name: 'chris', skill: [400, 400 / 3], rank: 3 },
+    { name: 'darren', skill: [400, 400 / 3], rank: 4 },
+    { name: 'elis', skill: [400, 400 / 3], rank: 5 },
   ]
   console.log(calc(users))
 }
